8d666b65d0bb1aa76dbfb97eac0edb5d4caacbff
prajwalkvp <prajwalkodur@gmail.com>
2020-08-21 17:16:05 +0530

    Updates

---
+++ precache-manifest.49a0fa1c97e4824ffbfa2a9c0bd2a6d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d4dfe9a2dbf71f300b2c5d962d992b",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "29eea0ed6fd2c7bd7ef0",
+    "url": "/React-todo/static/css/main.e59d9f09.chunk.css"
+  },
+  {
+    "revision": "19011f947fb9ee7c8bdc",
+    "url": "/React-todo/static/js/2.72f57c0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo/static/js/2.72f57c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29eea0ed6fd2c7bd7ef0",
+    "url": "/React-todo/static/js/main.4c8c1931.chunk.js"
+  },
+  {
+    "revision": "38d19433d83ae80b86ea",
+    "url": "/React-todo/static/js/runtime-main.8702ebd2.js"
+  }
+]);
